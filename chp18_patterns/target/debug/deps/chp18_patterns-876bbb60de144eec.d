2fc804deac4fe65070cb00e9e12047a6c6d776f1
foglet15 <emdort@gmail.com>
2022-10-16 16:03:20 -0400

    init chp 18

---
+++ chp18_patterns/target/debug/deps/chp18_patterns-876bbb60de144eec.d
@@ -0,0 +1,5 @@
+/home/foglet15/Code/Rust/RustProgrammingBook/projects/chp18_patterns/target/debug/deps/chp18_patterns-876bbb60de144eec.rmeta: src/main.rs
+
+/home/foglet15/Code/Rust/RustProgrammingBook/projects/chp18_patterns/target/debug/deps/chp18_patterns-876bbb60de144eec.d: src/main.rs
+
+src/main.rs:
---
+++ chp18_patterns/target/debug/deps/chp18_patterns-8f68bd6cfd7ff1cb.d
@@ -0,0 +1,5 @@
+/home/foglet15/Code/Rust/RustProgrammingBook/projects/chp18_patterns/target/debug/deps/chp18_patterns-8f68bd6cfd7ff1cb.rmeta: src/main.rs
+
+/home/foglet15/Code/Rust/RustProgrammingBook/projects/chp18_patterns/target/debug/deps/chp18_patterns-8f68bd6cfd7ff1cb.d: src/main.rs
+
+src/main.rs:
